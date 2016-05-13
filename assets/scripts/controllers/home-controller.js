124e5667f79acce52b1ac43c9052a17bc6a3335f
agoqui <ago.quintela@gmail.com>
2016-05-13 15:54:18 +0200

    gestion formulaire client, vol, synthese

--- assets/scripts/controllers/home-controller.js
+++ assets/scripts/controllers/home-controller.js
@@ -1,92 +1,96 @@
-mainApp.controller('HomeController', ['$scope',function($scope) { 
+mainApp.controller('HomeController', ['$scope', 'flightsList', function($scope,$flightsList) { 
 
-	$scope.titre = 'Recherche par ville';
+	$scope.custosynt=[];
+	$scope.custosyntFly=[];
 	$scope.custoDisabled = false;
 	$scope.fligths = [];
-	$scope.btnSalvador = true;
-	$scope.btnLisboa = true;
-	$scope.btnBuenoaires = true;
-	$scope.btnBarcelona = true;
 
+	$scope.fligths = $flightsList.getFlights();
+	$scope.custoSaved = false;
 
-	$scope.saveCustomer = function(){
-
-		
+	$scope.custoChange = function(){
+		$scope.custoSaved = false;
+	}
+	
+	$scope.saveCustomer = function(){		
 		//$scope.custoDisabled = true;
 		var re = new RegExp("^[0-9]{10}$");
-		console.log($scope.custo.email);
-		console.log(re.test($scope.custo.phone));
+		
 		if(!re.test($scope.custo.phone)) {
-			
 			$scope.custoForm.$valid = false;
 		}
 
-	};
-
-	$scope.validateFly = function(dest){
-
-		console.log(dest);
-		
-		$scope.fligths.push({'destination':dest});
-		switch(dest) {
-			case 'salvador':
-				$scope.carsalvador= "flyOKClass";
-				$scope.btnSalvador = false;
-				break;
-			case 'lisboa':
-				$scope.carlisboa= "flyOKClass";
-				$scope.btnLisboa = false;
-				break;
-			case 'barcelona':
-				$scope.carbarcelona= "flyOKClass";
-				$scope.btnBarcelona = false;
-				break;
-			case 'buenoaires':
-				$scope.carbuenoaires= "flyOKClass";
-				$scope.btnBuenoaires = false;
-				break;
-
+		if ($scope.custoForm.$valid) {
+			$scope.custosynt.name = $scope.custo.name;
+			$scope.custosynt.surname = $scope.custo.surname;
+			$scope.custosynt.phone = $scope.custo.phone;
+			$scope.custosynt.email = $scope.custo.email;
+			$scope.custoSaved = true;
 		}
-		
-
 
 	};
 
-	$scope.cancelFly = function(dest){
+	var classCarousselActive = "car0";
+	$scope[classCarousselActive]= "active";
 
-		var tmpfly = [];
-		$scope.fligths.forEach(function(val){
-			console.log( val);
-			if (val.destination != dest) tmpfly.push(val);
+	$scope.validateFly = function(index, indexFlight){
+
+		// Validate
+		if ($scope.fligths[index].flight[indexFlight].reserved == false) {
+			//$scope.fligths[index].reserved = true;
+			$scope.fligths[index].flight[indexFlight].reserved = true;
+			
+			
+			classCarousselActive = "car"+index;
+			$scope[classCarousselActive]= "flyOKClass active";
+			classFlightActive = "flight"+index+indexFlight;
+			$scope[classFlightActive]= "flyOKClass";
+			
+			
+			$scope.custosyntFly.push({"number": $scope.fligths[index].flight[indexFlight].number,
+				"indexTour" : index,
+				"indexVol" : indexFlight,
+				"name" : $scope.fligths[index].name,
+				"destination": $scope.fligths[index].destination,
+				"date" : $scope.fligths[index].flight[indexFlight].date
+			});
+	
+
+		} else {
+			var tmp = [];
+			$scope.custosyntFly.forEach(function(val){
+				if (val.number != $scope.fligths[index].flight[indexFlight].number) {
+					tmp.push(val);
+				}
+			});
+			$scope.custosyntFly = tmp;
+			//$scope.fligths[index].reserved = false;
+			$scope.fligths[index].flight[indexFlight].reserved = false;
+
+			classCarousselActive = "car"+index;
+			$scope[classCarousselActive]= "flyKOClass active";
+			
+			classFlightActive = "flight"+index+indexFlight;
+			$scope[classFlightActive]= "flyKOClass";
+				
+		}
+
+	};
+
+	
+	$scope.cancelFly = function(number, indexTour, indexVol){
+		var tmp = [];
+		$scope.custosyntFly.forEach(function(val){	
+			if (val.number != number) {
+				tmp.push(val);
+			}
 		});
-		$scope.fligths = tmpfly
-		console.log($scope.fligths);
-
-		
-		//$scope.fligths.push({'destination':dest});
-		switch(dest) {
-			case 'salvador':
-				$scope.carsalvador= "flyKOClass";
-				$scope.btnSalvador = true;
-				break;
-			case 'lisboa':
-				$scope.carlisboa= "flyKOClass";
-				$scope.btnLisboa = true;
-				break;
-			case 'barcelona':
-				$scope.carbarcelona= "flyKOClass";
-				$scope.btnBarcelona = true;
-				break;
-			case 'buenoaires':
-				$scope.carbuenoaires= "flyKOClass";
-				$scope.btnBuenoaires= true;
-				break;
-
-		}
+		$scope.custosyntFly = tmp;
+		$scope.fligths[indexTour].flight[indexVol].reserved = false;
+		classFlightActive = "flight"+indexTour+indexVol;
+		$scope[classFlightActive]= "flyKOClass";
 
 	};
-
-
 
 	
 }]);
